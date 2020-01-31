6514e1f40f997bb4335140f7a5accf329130c801
Gustav <sir.gustav.the.coder@gmail.com>
2020-01-31 21:30:38 +0100

    moved stuff and made really basic player

---
+++ unity/Assets/scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed { get; set; } = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dx = Input.GetAxis("Horizontal");
+        var p = this.transform.position;
+        this.transform.position = new Vector3(p.x + dx * Time.deltaTime * Speed, p.y, p.z);
+    }
+}
