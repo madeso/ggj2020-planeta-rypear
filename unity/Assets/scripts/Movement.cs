186ca64cedc9f25471603542235bd22d2ba732da
Gustav <sir.gustav.the.coder@gmail.com>
2020-01-31 22:02:09 +0100

    more advanced player controller, w/ things blocking player from exiting the frame

--- unity/Assets/scripts/Movement.cs
+++ unity/Assets/scripts/Movement.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     public float Speed { get; set; } = 10;
+
+    public float MovementSmoothing { get; set; } = .05f;
+
+    private Vector3 m_Velocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         var dx = Input.GetAxis("Horizontal");
-        var p = this.transform.position;
-        this.transform.position = new Vector3(p.x + dx * Time.deltaTime * Speed, p.y, p.z);
+
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(dx * Speed, rb.velocity.y);
+        // And then smoothing it out and applying it to the character
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, MovementSmoothing);
+
     }
 }
