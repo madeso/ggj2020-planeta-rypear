879d172d3e46190d189d71f28229a07b22f35fda
Gustav <sir.gustav.the.coder@gmail.com>
2020-02-01 21:51:45 +0100

    goal tweaks

--- unity/Assets/scripts/Goal.cs
+++ unity/Assets/scripts/Goal.cs
@@ -7,10 +7,33 @@ public class Goal : MonoBehaviour
     public int TrackIndex = 0;
 
     private MusicSong song;
+    private MeshRenderer mesh;
+
+    public MeshRenderer[] Partners;
+
+    IEnumerable<MeshRenderer> Meshes
+    {
+        get
+        {
+            if (this.mesh != null)
+            {
+                yield return this.mesh;
+            }
+            foreach(var m in this.Partners)
+            {
+                yield return m;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        this.mesh = GetComponent<MeshRenderer>();
+        foreach(var m in this.Meshes)
+        {
+            m.enabled = false;
+        }
         this.song = GameObject.FindObjectOfType<MusicSong>();
         if(this.song == null)
         {
@@ -20,12 +43,24 @@ public class Goal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        foreach(var m in Meshes)
+        {
+            if (m.enabled)
+            {
+                return;
+            }
+        }
         Debug.Log(string.Format("Goal collided with {0}", collision.gameObject.name));
 
         GameObject.Destroy(collision.gameObject);
-        GameObject.Destroy(this.gameObject);
 
-        if(this.song != null)
+        foreach(var m in Meshes)
+        {
+            m.enabled = true;
+        }
+
+
+        if (this.song != null)
         {
             this.song.DoCrossfade(this.TrackIndex);
         }
