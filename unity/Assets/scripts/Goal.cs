0ec3d19463f3cb7444b0fd814d24cf0fcb021687
Gustav <sir.gustav.the.coder@gmail.com>
2020-02-01 16:20:04 +0100

    added goal and basic block test

---
+++ unity/Assets/scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(string.Format("Goal collided with {0}", collision.gameObject.name));
+
+        GameObject.Destroy(collision.gameObject);
+        GameObject.Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
