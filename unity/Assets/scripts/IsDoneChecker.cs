cb2cb42d19283a6aab415318e038e94fc4a59e90
Gustav <sir.gustav.the.coder@gmail.com>
2020-02-02 00:16:17 +0100

    restart current scene when "done"

---
+++ unity/Assets/scripts/IsDoneChecker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IsDoneChecker : MonoBehaviour
+{
+    private MusicSong song;
+    public float TimeToNext = 3;
+
+    float cursor = 0;
+
+
+    void Start()
+    {
+        this.song = GameObject.FindObjectOfType<MusicSong>();
+        if (this.song == null)
+        {
+            Debug.Log(string.Format("IsDoneChecker {0} was unable to find song", this.gameObject.name));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.song == null) { return; }
+
+        if(this.song.IsDone == false)
+        {
+            return;
+        }
+
+        cursor += Time.deltaTime;
+
+        if(cursor > this.TimeToNext)
+        {
+            cursor = -42;
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
