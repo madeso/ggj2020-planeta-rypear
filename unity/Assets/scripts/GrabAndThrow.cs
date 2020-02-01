0a096de3532f2820742ea31b004845961a14087a
Gustav <sir.gustav.the.coder@gmail.com>
2020-02-01 14:39:07 +0100

    pickup and throw code

--- unity/Assets/scripts/GrabAndThrow.cs
+++ unity/Assets/scripts/GrabAndThrow.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class GrabAndThrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // carrying information
+    public GameObject ThingPosition;
+
+    // grab information
+    public GameObject HandPosition;
+    public float HandRadius = 5;
+    public string PickupLayerMaskName;
+
+    private LayerMask PickupLayerMask;
+
+
+    private void PickupItem()
     {
-        
+        if (this.HandPosition != null && this.HandRadius > 0)
+        {
+            var collided = Physics2D.OverlapCircle(this.HandPosition.transform.position, this.HandRadius, this.PickupLayerMask);
+            if (collided != null)
+            {
+                Debug.Log(string.Format("pickup thingy: {0}", collided.name));
+            }
+            else
+            {
+                Debug.Log("nothing");
+            }
+        }
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        this.PickupLayerMask = LayerMask.GetMask(this.PickupLayerMaskName);
+    }
+
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+            PickupItem();
+        }
+        // todo: throw item if carrying
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(this.HandPosition != null && this.HandRadius > 0)
+        {
+            Gizmos.DrawWireSphere(this.HandPosition.transform.position, this.HandRadius);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // todo: move item if carrying
     }
 }
